4cf3d47cd25168522e542a82f4439bd0c9233227
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-08-03 15:59:38 -0700

    created server and schema db

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routes = require("./routes");
+const pg = require("pg");
+const sequelize = require("sequelize");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
