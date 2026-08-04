4b1d17e22b2935f21f40d8151d7dfa9045ee1209
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2026-08-04 12:34:55 -0700

    Update connection.js
    Enabling Neon SSL connections

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,15 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
